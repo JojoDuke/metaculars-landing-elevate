d384b6e999410e22bd3c59cb4513d2f25311a57b
Jojo Duke <45549237+JojoDuke@users.noreply.github.com>
2024-12-19 23:20:52 +0000

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,7 +35,7 @@ const Navbar = () => {
           <div className="flex items-center justify-between">
             <div className="flex items-center">
               <img
-                src="/assets/logo.png"
+                src="/assets/logo-new.jpeg"
                 alt="Metaculars Logo"
                 className="h-10 w-auto"
               />
